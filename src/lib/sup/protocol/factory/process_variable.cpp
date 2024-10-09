81306c3a460b91b8b1c4d424782475ef77a5a926
torresr <rjtorres@criticalsoftware.com>
2024-10-09 10:00:15 +0000

    Pass capture vars by reference to lambda

--- src/lib/sup/protocol/factory/process_variable.cpp
+++ src/lib/sup/protocol/factory/process_variable.cpp
@@ -85,7 +85,7 @@ bool WaitForVariableValue(ProcessVariable& var, const sup::dto::AnyValue& value,
   std::unique_lock<std::mutex> lk(mtx);
   TryFetchVariable(var, current);
   return cv.wait_for(lk, std::chrono::nanoseconds(std::lround(timeout_sec * 1e9)),
-                     [current, value]() { return current == value; });
+                     [&current, &value]() { return current == value; });
 }
 
 }  // namespace protocol
