ef8e50b25640f9c12222f0e1d67b5341a5674efa
RTorres <rjtorres@criticalsoftware.com>
2025-01-03 12:34:41 +0000

    Define standard as c++17

--- cmake/module/configuration.cmake
+++ cmake/module/configuration.cmake
@@ -9,7 +9,7 @@ include(FindPackageMessage)
 # -----------------------------------------------------------------------------
 # C++ version
 # -----------------------------------------------------------------------------
-set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
