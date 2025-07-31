f7ece2bc7fc5b5b905f3318c80d7877908525cb1
Walter Van Herck <walter.vanherck@iter.org>
2025-07-31 13:25:43 +0000

    Add LogAnyValueFunctorDecorator

---
+++ src/lib/sup/protocol/decorators/log_anyvalue_functor_decorator.cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP RPC protocol stack
+ *
+ * Description   : The definition and implementation for the RPC protocol stack in SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/protocol/log_anyvalue_functor_decorator.h>
+
+#include <sup/dto/anyvalue.h>
+
+namespace sup
+{
+namespace protocol
+{
+
+LogAnyValueFunctorDecorator::LogAnyValueFunctorDecorator(
+  sup::dto::AnyFunctor& functor, LogFunction log_function)
+  : m_functor{functor}
+  , m_log_function{log_function}
+{}
+
+LogAnyValueFunctorDecorator::~LogAnyValueFunctorDecorator() = default;
+
+sup::dto::AnyValue LogAnyValueFunctorDecorator::operator()(const sup::dto::AnyValue& input)
+{
+  m_log_function(input, "Request network packet");
+  auto reply = m_functor(input);
+  m_log_function(reply, "Reply network packet");
+  return reply;
+}
+
+}  // namespace protocol
+
+}  // namespace sup
---
+++ src/lib/sup/protocol/log_anyvalue_functor_decorator.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP RPC protocol stack
+ *
+ * Description   : The definition and implementation for the RPC protocol stack in SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_PROTOCOL_LOG_ANYVALUE_FUNCTOR_DECORATOR_H_
+#define SUP_PROTOCOL_LOG_ANYVALUE_FUNCTOR_DECORATOR_H_
+
+#include <sup/dto/any_functor.h>
+
+#include <functional>
+
+namespace sup
+{
+namespace dto
+{
+class AnyValue;
+}  // namespace dto
+
+namespace protocol
+{
+
+class LogAnyValueFunctorDecorator : public sup::dto::AnyFunctor
+{
+public:
+  using LogFunction = std::function<void(const sup::dto::AnyValue&, const std::string&)>;
+  LogAnyValueFunctorDecorator(sup::dto::AnyFunctor& functor, LogFunction log_function);
+  ~LogAnyValueFunctorDecorator() override;
+
+  sup::dto::AnyValue operator()(const sup::dto::AnyValue& input) override;
+
+private:
+  sup::dto::AnyFunctor& m_functor;
+  LogFunction m_log_function;
+};
+
+}  // namespace protocol
+
+}  // namespace sup
+
+#endif  // SUP_PROTOCOL_PROTOCOL_H_
